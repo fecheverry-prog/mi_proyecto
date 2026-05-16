e3fa396ee30b22d5d4bc14f2fd60523245a71b6c
fecheverry-prog <federica.echeverry@gmail.com>
2026-05-16 15:04:21 +0000

    Agrego funcion de cuadrados

--- mi_proyecto/main.py
+++ mi_proyecto/main.py
@@ -2,4 +2,5 @@ from src.operaciones import filtrar_pares
 print('Pares', filtrar_pares([1,2,3,4,5,6]))from src.operaciones import lista_al_cuadrado
 print('Cuadradps', lista_al_cuadrado([1,2,3,4,5,6]))from src.operaciones import lista_al_cuadrado
 print('Cuadrados', lista_al_cuadrado([1,2,3,4,5,6]))from src.operaciones import lista_al_cuadrado
+print('Cuadrados', lista_al_cuadrado([1,2,3,4,5,6]))from src.operaciones import lista_al_cuadrado
 print('Cuadrados', lista_al_cuadrado([1,2,3,4,5,6]))
